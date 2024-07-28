88de318c44a95d4899e5102ce09772ab348b68b9
Ashfaque_Ansari <ashfaqueansari101@gmail.com>
2024-07-28 13:43:51 +0530

    added React-Weather-App-Main

---
+++ src/components/Credit.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Coded by Mohammad Ashfaque Ansari
+      <a href="https://www.linkedin.com/in/mohammad-ashfaque-ansari-029a1225b/"/>
+  
+    </p>
+  </div>
+  );
+}
